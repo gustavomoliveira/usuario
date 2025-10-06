9b3016fae98407be825621adda8f6e390f41f7cb
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2025-10-06 18:45:48 -0300

    adicionados métodos get, delete e login

---
+++ src/main/java/com/gustavodeoliveira/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.gustavodeoliveira.praticando_spring.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
